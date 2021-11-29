4a3cdc86761bb6f007e15b528a5f8ee87630e343
Lakdar Arfento <arfento@gmail.com>
2021-11-29 16:06:40 +0700

    commit all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'TestNgProject';
+
+  
+export class AppComponent  {
+
+  title = "QR Code"
+  public myAngularxQrCode: string = "";
+  
+  constructor () {
+    this.myAngularxQrCode = 'ItSoluionStuff.com';
+  }
+  // @ViewChild("mapContainer", { static: false }) gmap: ElementRef;
+  // map: google.maps.Map;
+  // lat = -6.1753924;
+  // lng = 106.8271528;
+
+  // markers = [
+  //   {
+  //     position: new google.maps.LatLng(40.73061, 73.935242),
+  //     map: this.map,
+  //     title: "Marker 1"
+  //   },
+  //   {
+  //     position: new google.maps.LatLng(32.06485, 34.763226),
+  //     map: this.map,
+  //     title: "Marker 2"
+  //   }
+  // ];
+
+  // //Coordinates to set the center of the map
+  // coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  // mapOptions: google.maps.MapOptions = {
+  //   center: this.coordinates,
+  //   zoom: 8
+  // };
+
+  // //Default Marker
+  // marker = new google.maps.Marker({
+  //   position: this.coordinates,
+  //   map: this.map,
+  //   title: "Hello World!"
+  // });
+
+  // ngAfterViewInit(): void {
+  //   this.mapInitializer();
+  // }
+
+  // mapInitializer(): void {
+  //   this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+
+  //   //Adding Click event to default marker
+  //   this.marker.addListener("click", () => {
+  //     const infoWindow = new google.maps.InfoWindow({
+  //       content: this.marker.getTitle()
+  //     });
+  //     infoWindow.open(this.marker.getMap(), this.marker);
+  //   });
+
+  //   //Adding default marker to map
+  //   this.marker.setMap(this.map);
+
+  //   //Adding other markers
+  //   this.loadAllMarkers();
+  // }
+
+  // loadAllMarkers(): void {
+  //   this.markers.forEach(markerInfo => {
+  //     //Creating a new marker object
+  //     const marker = new google.maps.Marker({
+  //       ...markerInfo
+  //     });
+
+  //     //creating a new info window with markers info
+  //     const infoWindow = new google.maps.InfoWindow({
+  //       content: marker.getTitle()
+  //     });
+
+  //     //Add click event to open info window on marker
+  //     marker.addListener("click", () => {
+  //       infoWindow.open(marker.getMap(), marker);
+  //     });
+
+  //     //Adding marker to google map
+  //     marker.setMap(this.map);
+  //   });
+  // }
 }
